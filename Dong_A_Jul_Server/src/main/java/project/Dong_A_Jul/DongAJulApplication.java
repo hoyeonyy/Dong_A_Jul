4e0497fbdbb51e016e0659d433ee369dd81c6321
hoyeon <highoption@naver.com>
2023-08-03 20:55:32 +0900

    inital project

---
+++ Dong_A_Jul_Server/src/main/java/project/Dong_A_Jul/DongAJulApplication.java
@@ -0,0 +1,13 @@
+package project.Dong_A_Jul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongAJulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongAJulApplication.class, args);
+	}
+
+}
---
+++ Dong_A_Jul_Server/src/test/java/project/Dong_A_Jul/DongAJulApplicationTests.java
@@ -0,0 +1,13 @@
+package project.Dong_A_Jul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongAJulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

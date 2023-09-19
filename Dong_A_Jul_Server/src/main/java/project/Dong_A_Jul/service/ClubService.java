de3ceb719325b38bdcb93b361814c067c41085d7
hoyeon <highoption@naver.com>
2023-09-19 18:15:39 +0900

    프로토타입 제작2

---
+++ Dong_A_Jul_Server/src/main/java/project/Dong_A_Jul/service/ClubService.java
@@ -0,0 +1,13 @@
+package project.Dong_A_Jul.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ClubService {
+
+
+
+}
